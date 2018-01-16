177eb4848ae8ce078992b13fc9f44f896d1bc6e2
Alevtina Lobova <ally1125@gmail.com>
2018-01-16 01:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return(`Happy holidays, ${name}!`);
 }
-happyHolidaysTo(you)
+
 
 function happyHolidayTo(holiday,name){
   return(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(days,holiday){
+  return(`It's ${days} days until ${holiday}!`)
 }
